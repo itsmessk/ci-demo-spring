a6bff58bb2c50155a1cb463d268013a3d1a7403d
Sundar Karthik S <sundar1007karthik@gmail.com>
2026-03-11 13:42:58 +0530

    first commit

---
+++ src/main/java/com/ci/sprincidemo/SprinCiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ci.sprincidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinCiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinCiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ci/sprincidemo/SprinCiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ci.sprincidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinCiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
